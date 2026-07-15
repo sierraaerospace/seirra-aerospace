790463f5e998f721b3166ae2aefa3d2cb7e45a3f
sierraaerospace <sierraaerospace@gmail.com>
2026-07-15 12:10:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,56 +4,32 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import { CartProvider } from "./contexts/CartContext";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Login from "./pages/Login";
-import Cart from "./pages/Cart";
-import OrderHistory from "./pages/OrderHistory";
 import Warranty from "./pages/Warranty";
 import ShippingReturns from "./pages/ShippingReturns";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import ConditionsOfSale from "./pages/ConditionsOfSale";
-import AuthCallback from "./pages/AuthCallback";
-import Settings from "./pages/Settings";
-import Profile from "./pages/Profile";
-import { useAuthCodeExchange } from "@/hooks/useAuthCodeExchange";
 
 const queryClient = new QueryClient();
-
-const AuthCodeExchange = () => {
-  useAuthCodeExchange();
-  return null;
-};
 
 const App = () => (
   <HelmetProvider>
     <QueryClientProvider client={queryClient}>
-      <CartProvider>
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            {/* Ensures OAuth code exchange works even if provider redirects to / */}
-            <AuthCodeExchange />
-
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/auth/callback" element={<AuthCallback />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/orders" element={<OrderHistory />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/warranty" element={<Warranty />} />
-              <Route path="/shipping-returns" element={<ShippingReturns />} />
-              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-              <Route path="/conditions-of-sale" element={<ConditionsOfSale />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </TooltipProvider>
-      </CartProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/warranty" element={<Warranty />} />
+            <Route path="/shipping-returns" element={<ShippingReturns />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/conditions-of-sale" element={<ConditionsOfSale />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
     </QueryClientProvider>
   </HelmetProvider>
 );
